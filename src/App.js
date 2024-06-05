4f55135d2a5e9e4d1acdcd5dd6e57bdf2b194643
Arfara Yema S <indonesianstudent14@gmail.com>
2024-06-05 23:19:47 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import DinoGame from './DinoGame';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Intan Loncat
+      </h1>
+      <DinoGame />
     </div>
   );
-}
+};
 
 export default App;
